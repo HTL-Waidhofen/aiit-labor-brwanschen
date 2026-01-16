b4d068c01ffef4be6d5b52517e7954fe2249950e
Daniel Maurer <daniel.maurer@htlwy.at>
2026-01-16 12:35:31 +0100

    Updated

--- Lab04_Bruchrechnung/Lab04_Bruchrechnung/Bruchrechnung.cs
+++ Lab04_Bruchrechnung/Lab04_Bruchrechnung/Bruchrechnung.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,18 @@ namespace Lab04_Bruchrechnung
         Bruch b1;
         Bruch b2;
 
+        public static Bruchrechnung Parse(string str)
+        {
+            String[] parts = str.Split(new char[] { '+', '-', '*', ':' });
+            Bruch b1 = Bruch.Parse(parts[0]);
+            Bruch b2 = Bruch.Parse(parts[1]);
+            return new Bruchrechnung(b1, b2);
+        }
+
+        public Bruch getResult()
+        {
+
+        }
 
 
 
